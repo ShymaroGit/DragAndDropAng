d76fa96d6d3ad58011793adb7c4cf8953d1b13b9
ShymaroGit <44645171+ShymaroGit@users.noreply.github.com>
2023-06-08 03:37:09 -0400

    First attempt - kinda working

--- DragAndDrop/src/app/nav/nav.component.ts
+++ DragAndDrop/src/app/nav/nav.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  pos1 = 0;
+  pos2 = 0;
+  pos3 = 0;
+  pos4 = 0;
+  top = 0;
 
+  ngOnInit(): void {
+    this.dragElement(document.getElementById('mydiv'));
+  }
+
+  dragElement(elmnt: any) {
+    if (document.getElementById(elmnt.id + 'header')) {
+      // if present, the header is where you move the DIV from:
+      document.getElementById(elmnt.id + 'header')!.onmousedown =
+        this.dragMouseDown;
+    } else {
+      // otherwise, move the DIV from anywhere inside the DIV:
+      elmnt.onmousedown = this.dragMouseDown;
+    }
+  }
+
+  dragMouseDown(e: any) {
+    console.log('mouse down');
+    e = e || window.event;
+    e.preventDefault();
+
+    this.pos3 = e.clientX;
+    this.pos4 = e.clientY;
+    console.log(this.pos3);
+
+    document.onmouseup = this.closeDragElement;
+    document.onmousemove = this.elementDrag;
+  }
+
+  elementDrag(e: any) {
+    console.log('this.pos3');
+    e = e || window.event;
+    e.preventDefault();
+
+    this.pos1 = this.pos3 - e.clientX;
+    this.pos2 = this.pos4 - e.clientY;
+    this.pos3 = e.clientX;
+    this.pos4 = e.clientY;
+
+    let elmnt = document.getElementById('mydiv');
+    let offTop = elmnt!.offsetTop - this.pos2;
+    let offLeft = elmnt!.offsetLeft - this.pos1;
+    elmnt!.style.top = offTop + 'px';
+    elmnt!.style.left = offLeft + 'px';
+  }
+
+  closeDragElement() {
+    console.log('t');
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
